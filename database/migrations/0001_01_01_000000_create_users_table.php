4e2787e8c6d3588710e166d5a3da5ce15de7b83e
shermy0 <shermydihya98@gmail.com>
2025-10-09 11:33:11 +0700

    migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable(); // tambahan
+            $table->enum('role', ['provider', 'client', 'admin'])->default('client'); // tambahan
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_10_09_041616_create_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('bio')->nullable();
+            $table->string('skill')->nullable();
+            $table->string('location')->nullable();
+            $table->string('photo')->nullable();
+            $table->decimal('rate', 10, 2)->nullable();
+            $table->string('portfolio')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
